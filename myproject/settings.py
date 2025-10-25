f981ce944ae0f4c9cbdbd6e4a355f263509a9bb9
hanova-cmd <nazerkeaitahanova@gmail.com>
2025-10-25 22:14:46 +0500

    Add STATIC_ROOT setting

--- myproject/settings.py
+++ myproject/settings.py
@@ -72,3 +72,10 @@ STATICFILES_DIRS = [BASE_DIR / 'theme' / 'static']
 TAILWIND_APP_NAME = 'theme'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
